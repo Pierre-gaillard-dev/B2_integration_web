baa2218733997878df4ed3c3af3b06a80cbcfc33
Pierre <pierre.gaillard56@orange.fr>
2024-09-25 15:49:33 +0200

    minor fixes

--- src/navbar.js
+++ src/navbar.js
@@ -1,37 +1,31 @@
-var nav_menu = {
-    opened : false,
+const nav_menu = {
+    opened: false,
+    pop_up: null,
 
-    pop_up : document.getElementById("nav_bar_popup"),
-
-    toogle : function() {
-        if (this.opened) {
-            this.close()
-        } else {
-            this.open()
-        }
-        //console.log(this.opened)
+    toggle() {
+        this.opened ? this.close() : this.open();
     },
 
-    open : function() {
-        this.opened = true
-        //console.log(this.pop_up.classList)
-        this.pop_up.classList.remove("closed")
-        this.pop_up.classList.add("opened")
+    open() {
+        this.opened = true;
+        this.updateClass();
     },
 
-    close : function() {
-        this.opened = false
-        this.pop_up.classList.remove("opened")
-        this.pop_up.classList.add("closed")
+    close() {
+        this.opened = false;
+        this.updateClass();
     },
 
-    init : function() {
+    updateClass() {
+        this.pop_up.classList.toggle("opened", this.opened);
+        this.pop_up.classList.toggle("closed", !this.opened);
+    },
+
+    init() {
         this.pop_up = document.getElementById("nav_bar_popup");
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    nav_menu.pop_up = document.getElementById("nav_bar_popup")
-    console.log(nav_menu.pop_up)
+document.addEventListener('DOMContentLoaded', () => {
+    nav_menu.init();
 });
-console.log("test")
